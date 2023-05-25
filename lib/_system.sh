58f1a037df5b8bddf84f506062bf5da8772e5cab
victormartinsvm <victorwanderson28@gmail.com>
2023-05-25 17:18:03 -0300

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -29,7 +29,7 @@ EOF
 #######################################
 system_git_clone() {
   print_banner
-  printf "${WHITE} 💻 Fazendo download do código FireZAP...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Fazendo download do código Zaplay...${GRAY_LIGHT}"
   printf "\n\n"
 
 
@@ -49,7 +49,7 @@ EOF
 #######################################
 system_update() {
   print_banner
-  printf "${WHITE} 💻 Vamos atualizar o sistema FireZAP...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos atualizar o sistema Zaplay...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -71,7 +71,7 @@ EOF
 #######################################
 deletar_tudo() {
   print_banner
-  printf "${WHITE} 💻 Vamos deletar o FireZAP...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos deletar o Zaplay...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -117,7 +117,7 @@ EOF
 #######################################
 configurar_bloqueio() {
   print_banner
-  printf "${WHITE} 💻 Vamos bloquear o FireZAP...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos bloquear o Zaplay...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -144,7 +144,7 @@ EOF
 #######################################
 configurar_desbloqueio() {
   print_banner
-  printf "${WHITE} 💻 Vamos Desbloquear o FireZAP...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos Desbloquear o Zaplay...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -170,7 +170,7 @@ EOF
 #######################################
 configurar_dominio() {
   print_banner
-  printf "${WHITE} 💻 Vamos Alterar os Dominios do FireZAP...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Vamos Alterar os Dominios do Zaplay...${GRAY_LIGHT}"
   printf "\n\n"
 
 sleep 2
